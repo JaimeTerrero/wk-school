99241008a91dcaa330c77e97c48b16c1504d1a1d
JaimeTerrero <jaimedavidterre@gmail.com>
2024-03-20 18:26:54 -0400

    user and navbar components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserCreateComponent } from './Pages/user-create/user-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user/create', component: UserCreateComponent, title: 'Create User'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbardComponent } from './Pages/Partials/navbard/navbard.component';
+import { UserCreateComponent } from './Pages/user-create/user-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbardComponent,
+    UserCreateComponent
   ],
   imports: [
     BrowserModule,
